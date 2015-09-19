0f389ba2d09b362b8e89811c8406e760df8ba4e0
James Church <jcchurch@gmail.com>
2015-09-19 12:29:37 -0400

    Add presentation

--- MyShapes3.py
+++ MyShapes3.py
@@ -2,7 +2,7 @@ from shapes import *
 
 window = createCanvas(600, 600)
 
-drawFilledRectangle(window, 25, 50, 125, 150, 'orange')
+drawFilledRectangle(window, 25, 50, 125, 150, 'lightblue')
 drawFilledCircle(window, 200, 100, 50, 'blue')
 drawFilledTriangle(window, 325, 50, 100, 'green')
 
