27efc44476849e5979911a70e1252c8afe924912
James Church <jcchurch@gmail.com>
2015-09-19 10:08:43 -0400

    Add MyTurtle2.py (Triangle)

---
+++ MyTurtle2.py
@@ -0,0 +1,17 @@
+import turtle
+
+window = turtle.Screen()
+
+t = turtle.Turtle()
+t.shape('turtle')
+t.color('green', 'yellow')
+
+t.down()
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+window.mainloop()
