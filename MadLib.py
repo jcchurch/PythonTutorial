a9c53e3aa9b879a761704f165ba93bcc5d34a9c1
James Church <jcchurch@gmail.com>
2015-09-19 08:33:47 -0400

    Add MadLib.py

---
+++ MadLib.py
@@ -0,0 +1,6 @@
+person = input('A person: ')
+noun = input('A noun: ')
+verb = input('A verb: ')
+print('One day,',person,'went to a party.')
+print('A big mean',noun,'stood in their way.')
+print('So',person,'did a ',verb,'and as able to get past!')
