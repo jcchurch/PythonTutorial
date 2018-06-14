da9fcd6b58256e3c96d638d1c59bd5ed7d73c4a0
James Church <jcchurch@gmail.com>
2018-06-13 20:40:12 -0500

    Adds game of life animation code

---
+++ GameOfLife/GameOfLife.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+ON = 255
+OFF = 0
+
+def update(frameNum, img, grid, N):
+    newGrid = grid.copy()
+    for i in range(N):
+        for j in range(N):
+            total = (  grid[(i-1)%N,(j-1)%N]
+                    + grid[(i-1)%N,j]
+                    + grid[(i-1)%N,(j+1)%N]
+                    + grid[i,(j-1)%N]
+                    + grid[i,(j+1)%N]
+                    + grid[(i+1)%N,(j-1)%N]
+                    + grid[(i+1)%N,j]
+                    + grid[(i+1)%N,(j+1)%N])/255
+
+            if grid[i,j] == ON and (total < 2 or total > 3):
+                newGrid[i,j] = OFF
+            elif grid[i,j] == OFF and total == 3:
+                newGrid[i,j] = ON
+
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return img,
+
+def main():
+    N = 100
+    grid = np.random.choice([OFF,ON], (N, N))
+
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update,
+                                  fargs=(img, grid, N, ),
+                                  frames=1000,
+                                  save_count=50)
+    plt.show()
+
+main()
